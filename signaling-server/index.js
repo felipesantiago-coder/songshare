937ae34d2765943f9e1dc8d0387f7c5f6374a849
felipesantiago-coder <felipesantiagoquadra@gmail.com>
2026-05-11 23:52:56 -0300

    add_railway

---
+++ signaling-server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const { ExpressPeerServer } = require('peer');
+
+const app = express();
+const server = require('http').createServer(app);
+
+app.use(cors({ origin: '*', methods: ['GET', 'POST'] }));
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: '/peerjs'
+});
+
+app.use('/peerjs', peerServer);
+
+app.get('/', (req, res) => res.send('SongShare Signaling Online 🚀'));
+
+const PORT = process.env.PORT || 9000;
+server.listen(PORT, () => console.log(`🟢 Servidor rodando na porta ${PORT}`));
