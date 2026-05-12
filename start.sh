842f5244679b6dab6e3a5c4501dfab7c8a5c761d
felipesantiago-coder <felipesantiagoquadra@gmail.com>
2026-05-12 01:23:24 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+set -e # Para o script se houver erro
+
 echo "🚀 Iniciando instalação das dependências..."
-npm install --production
+npm install --omit=dev
+
 echo "✅ Instalação concluída. Iniciando o servidor de sinalização..."
+# Certifique-se que o caminho para o arquivo do servidor está correto
 node server-sinalizacao.js
